a078ad4f95da75284f20ca2172bdf2c90f304223
Kazuhiro Sera <seratch@gmail.com>
2012-03-06 07:48:11 +0900

    Added converter

--- lib/scaruby.rb
+++ lib/scaruby.rb
@@ -1,6 +1,7 @@
 # -*- encoding: utf-8 -*-
 
 require 'scaruby/appliable_proc'
+require 'scaruby/converter'
 require 'scaruby/map'
 require 'scaruby/no_such_element_exception'
 require 'scaruby/option'
--- spec/scaruby_spec.rb
+++ spec/scaruby_spec.rb
@@ -1,7 +1,10 @@
 # -*- encoding: utf-8 -*-
 
+require 'scaruby'
+
 describe Scaruby do
-  it 'is available' do
+  it 'has converter' do
+    [1,2,3].to_scaruby.should_not eq(nil)
   end
 end
 
